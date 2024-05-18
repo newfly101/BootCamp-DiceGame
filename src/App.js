68d17eb6dee884d3dfa919c9e45f44401e3b3cfd
Jae-Hong-Kim <newfly101@naver.com>
2024-05-19 02:55:08 +0900

    default css setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+    <main>
+        <section className="player player--0">
+            <h2 className="name" id="name--0">Player 1</h2>
+            <p className="score" id="score--0">0</p>
+            <div className="current">
+                <p className="current-label">Current</p>
+                <p className="current-score" id="current--0">0</p>
+            </div>
+        </section>
+        <section className="player player--1 player--active">
+            <h2 className="name" id="name--1">Player 2</h2>
+            <p className="score" id="score--1">0</p>
+            <div className="current">
+                <p className="current-label">Current</p>
+                <p className="current-score" id="current--1">0</p>
+            </div>
+        </section>
+        <img src={`${process.env.PUBLIC_URL}/assets/dice3.png`} alt="Playing dice" className="dice"/>
+        <button className="btn btn--new">🔄 New game</button>
+        <button className="btn btn--roll">🎲 Roll dice</button>
+        <button className="btn btn--hold">📥 Hold</button>
+    </main>
+    </body>
   );
 }
 
